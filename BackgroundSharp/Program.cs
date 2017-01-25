29858398aea2b6980d66198c1bc9cb6a350e5b8f
Greg Dorr <dorrg@augsburg.edu>
2017-01-25 16:04:15 -0600

    Final Push

---
+++ BackgroundSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace BackgroundSharp
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern bool SystemParametersInfoW(uint uiAction, uint uiParam, IntPtr pvParam, uint fWinIni);
+
+        static void Main(string[] args)
+        {
+
+            IntPtr bptr = Marshal.StringToCoTaskMemUni(Path.Combine(Environment.CurrentDirectory, "TEST.png"));
+
+            SystemParametersInfoW(0x0014, 0, bptr, 0x0001);
+
+            Marshal.FreeCoTaskMem(bptr);
+        }
+    }
+}
